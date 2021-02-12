cbe8245ef77b43edf1d3b151564c1e9d598d362a
Daniel Vartanian <danvartan@gmail.com>
2021-02-12 10:50:39 -0300

    Fix codecov routine

--- tests/testthat/test-utils-mocks.R
+++ tests/testthat/test-utils-mocks.R
@@ -7,6 +7,6 @@ test_that("is_namespace_loaded() | general test", {
     expect_equal(is_namespace_loaded(x), isNamespaceLoaded(x))
 })
 
-# test_that("read_line() | general test", {
-#     expect_equal(read_line(""), "")
-# })
+test_that("read_line() | general test", {
+    expect_equal(read_line(""), "")
+})
